69aace3e7518e4cdb315238d6969542de8508357
alexmancilla <57542953+alexmancilla@users.noreply.github.com>
2022-05-28 01:30:18 -0500

    Add files via upload
    Dinosaur Class, working in POO with Java

---
+++ Dinosaur.java
@@ -0,0 +1,16 @@
+public class Dinosaur {
+  boolean isExtinct;
+  
+  public Dinosaur(boolean extinct) {
+    isExtinct = extinct;
+  }
+  
+  public static void main(String[] args) {
+    // program tasks
+    int dinoEnd = -2000000;
+    int actual = 2020;
+    boolean age = dinoEnd > actual;
+    Dinosaur triceratops = new Dinosaur(age);
+    System.out.println("The statment: Triceratops are alive today is -> " + triceratops.isExtinct);
+  }
+}
